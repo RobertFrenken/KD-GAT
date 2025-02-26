3d3e3fd15e0d48df785c4e0cb2f0d1cd5767229a
Robert Frenken <57292773+RobertFrenken@users.noreply.github.com>
2025-02-26 08:06:38 -0500

    Ran models

--- models/models.py
+++ models/models.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import GCNConv
-
+from torch_geometric.nn import global_mean_pool
 class SimpleGNN(torch.nn.Module):
     def __init__(self, input_dim, hidden_dim, output_dim):
         super(SimpleGNN, self).__init__()
@@ -42,6 +42,35 @@ class SimpleLSTM(nn.Module):
         # Index hidden state of last time step
         out = self.fc(out[:, -1, :])
         return out
+
+class AnomalyGNN(nn.Module):
+    def __init__(self, num_node_features, hidden_channels):
+        super(AnomalyGNN, self).__init__()
+        self.conv1 = GCNConv(num_node_features, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.classifier = nn.Linear(hidden_channels, 1)
+
+    def forward(self, x, edge_index):
+        x = F.relu(self.conv1(x, edge_index))
+        x = F.relu(self.conv2(x, edge_index))
+        x = global_mean_pool(x, batch)  # Aggregate node features
+        return self.classifier(x).squeeze()
+
+class GNN(torch.nn.Module):
+    def __init__(self, in_channels, hidden_channels, out_channels):
+        super(GNN, self).__init__()
+        self.conv1 = GCNConv(in_channels, hidden_channels)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.fc = torch.nn.Linear(hidden_channels, out_channels)
+
+    def forward(self, data):
+        x, edge_index, batch = data.x, data.edge_index, data.batch
+        x = self.conv1(x, edge_index)
+        x = F.relu(x)
+        x = self.conv2(x, edge_index)
+        x = global_mean_pool(x, batch)  # Global mean pooling
+        x = self.fc(x)
+        return x
 
 
 if __name__ == '__main__':
